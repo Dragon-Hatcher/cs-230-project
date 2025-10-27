f3fd3588300c13bd25c369fdd212c4bf2d27c5fb
Dragon-Hatcher <danieldragonhatcher@gmail.com>
2025-10-27 16:58:23 -0700

    Assign to training groups

---
+++ load_data.py
@@ -0,0 +1,92 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+SHOT_ID_DICT = {
+    7: 1,  8: 2,  9: 3, 16: 4, 17: 5, 18: 6, 19: 7, 20: 8, 21: 9, 22: 10,
+}
+STONE_BEING_SHOT_DICT = {
+    7: 2,  8: 8,  9: 3, 16: 9, 17: 4, 18: 10, 19: 5, 20: 11, 21: 6, 22: 12,
+}
+SHOT_TYPES_DICT = {
+    "Draw": 0,
+    "Front": 1,
+    "Guard": 2,
+    "Raise / Tap-back": 3,
+    "Wick / Soft Peeling": 4,
+    "Freeze": 5,
+    "Take-out": 6,
+    "Hit and Roll": 7,
+    "Clearing": 8,
+    "Double Take-out": 9,
+    "Promotion Take-out": 10,
+    "through": 11,
+}
+
+def load_data():
+    stones = pd.read_csv("dataset/Stones.csv")
+    ends = pd.read_csv("dataset/Ends.csv")
+
+    ends["PowerPlay"] = ends["PowerPlay"].fillna(0)
+    ends = ends.sort_values(by=["CompetitionID","SessionID","GameID","TeamID","EndID"])
+
+    ends["RunningScoreInclusive"] = ends.groupby(["CompetitionID","SessionID","GameID","TeamID"])["Result"].cumsum()
+    ends["RunningScoreExclusive"] = ends["RunningScoreInclusive"] - ends["Result"]
+
+    training_data = pd.DataFrame()
+
+    # [ 7  8  9 16 17 18 19 20 21 22]
+
+    stones["ShotID"] = stones["ShotID"].replace(SHOT_ID_DICT)
+    stones["StoneBeingShot"] = stones["ShotID"].replace(STONE_BEING_SHOT_DICT)
+    stones["IsHammer"] = stones["ShotID"] % 2 == 0
+
+    for i in range(1, 13):
+        stones[f"stone_{i}_thrown"] = stones[f"stone_{i}_x"] > 0
+        stones[f"ps_stone_{i}_thrown"] = stones[f"stone_{i}_thrown"] & (stones["StoneBeingShot"] != i)
+
+    for i in range(1, 7):
+        for var in ["x", "y", "thrown"]:
+            stones[f"ps_stone_shooter_{i}_{var}"] = \
+                stones[f"stone_{i}_{var}"] * (1 - stones["IsHammer"]) + \
+                stones[f"stone_{i+6}_{var}"] * stones["IsHammer"]
+        for var in ["x", "y", "thrown"]:
+            stones[f"ps_stone_opponent_{i}_{var}"] = \
+                stones[f"stone_{i}_{var}"] * stones["IsHammer"] + \
+                stones[f"stone_{i+6}_{var}"] * (1 - stones["IsHammer"])
+
+        # stones[f"stone_{i}_on_ice"] = stones[f"stone_{i}_x"] != 4095
+
+    # stones = stones.join(ends, on=["CompetitionID","SessionID","GameID","TeamID","EndID"])
+    stones = pd.merge(stones, ends, how="left", on=["CompetitionID","SessionID","GameID","TeamID","EndID"])
+
+    # ------------ Ids -------------------------
+    training_data["CompetitionID"] = stones["CompetitionID"]
+    training_data["SessionID"] = stones["SessionID"]
+    training_data["GameID"] = stones["GameID"]
+
+    # ------------ X Values ---------------------
+
+    training_data["EndID"] = stones["EndID"]
+    training_data["ShotID"] = stones["ShotID"]
+    training_data["IsHammer"] = stones["IsHammer"]
+    for i in range(1, 7):
+        for var in ["x", "y", "thrown"]:
+            training_data[f"ps_stone_shooter_{i}_{var}"] = stones[f"ps_stone_shooter_{i}_{var}"]
+            training_data[f"ps_stone_opponent_{i}_{var}"] = stones[f"ps_stone_opponent_{i}_{var}"]
+    training_data["ShooterScore"] = stones["RunningScoreExclusive"]
+    training_data["ShooterPowerPlay"] = stones["PowerPlay"]
+
+    up = stones["RunningScoreExclusive"].shift(1)
+    down = stones["RunningScoreExclusive"].shift(-1)
+    training_data["OpponentScore"] = np.where((stones.index + 1) % 10 == 0, up, down)
+
+    up = stones["PowerPlay"].shift(1)
+    down = stones["PowerPlay"].shift(-1)
+    training_data["OpponentPowerPlay"] = np.where((stones.index + 1) % 10 == 0, up, down)
+
+    # ------------ Y Values ---------------------
+    for name, val in SHOT_TYPES_DICT.items():
+        training_data[f"ShotType_{name}"] = stones["Task"] == val
+
+    return training_data
--- main.py
+++ main.py
@@ -1,68 +1,3 @@
-import tensorflow as tf
-import numpy as np
-import pandas as pd
-
-SHOT_ID_DICT = {
-    7: 1,  8: 2,  9: 3, 16: 4, 17: 5, 18: 6, 19: 7, 20: 8, 21: 9, 22: 10,
-}
-STONE_BEING_SHOT_DICT = {
-    7: 2,  8: 8,  9: 3, 16: 9, 17: 4, 18: 10, 19: 5, 20: 11, 21: 6, 22: 12,
-}
-
-
-stones = pd.read_csv("dataset/Stones.csv")
-ends = pd.read_csv("dataset/Ends.csv")
-
-ends["PowerPlay"] = ends["PowerPlay"].fillna(0)
-ends = ends.sort_values(by=["CompetitionID","SessionID","GameID","TeamID","EndID"])
-
-ends["RunningScoreInclusive"] = ends.groupby(["CompetitionID","SessionID","GameID","TeamID"])["Result"].cumsum()
-ends["RunningScoreExclusive"] = ends["RunningScoreInclusive"] - ends["Result"]
-
-training_data = pd.DataFrame()
-
-# [ 7  8  9 16 17 18 19 20 21 22]
-
-stones["ShotID"] = stones["ShotID"].replace(SHOT_ID_DICT)
-stones["StoneBeingShot"] = stones["ShotID"].replace(STONE_BEING_SHOT_DICT)
-stones["IsHammer"] = stones["ShotID"] % 2 == 0
-
-for i in range(1, 13):
-    stones[f"stone_{i}_thrown"] = stones[f"stone_{i}_x"] > 0
-    stones[f"ps_stone_{i}_thrown"] = stones[f"stone_{i}_thrown"] & (stones["StoneBeingShot"] != i)
-
-for i in range(1, 7):
-    for var in ["x", "y", "thrown"]:
-        stones[f"ps_stone_shooter_{i}_{var}"] = \
-            stones[f"stone_{i}_{var}"] * (1 - stones["IsHammer"]) + \
-            stones[f"stone_{i+6}_{var}"] * stones["IsHammer"]
-    for var in ["x", "y", "thrown"]:
-        stones[f"ps_stone_opponent_{i}_{var}"] = \
-            stones[f"stone_{i}_{var}"] * stones["IsHammer"] + \
-            stones[f"stone_{i+6}_{var}"] * (1 - stones["IsHammer"])
-
-    # stones[f"stone_{i}_on_ice"] = stones[f"stone_{i}_x"] != 4095
-
-# stones = stones.join(ends, on=["CompetitionID","SessionID","GameID","TeamID","EndID"])
-stones = pd.merge(stones, ends, how="left", on=["CompetitionID","SessionID","GameID","TeamID","EndID"])
-
-training_data["EndID"] = stones["EndID"]
-training_data["ShotID"] = stones["ShotID"]
-training_data["IsHammer"] = stones["IsHammer"]
-for i in range(1, 7):
-    for var in ["x", "y", "thrown"]:
-        training_data[f"ps_stone_shooter_{i}_{var}"] = stones[f"ps_stone_shooter_{i}_{var}"]
-        training_data[f"ps_stone_opponent_{i}_{var}"] = stones[f"ps_stone_opponent_{i}_{var}"]
-training_data["RunningScoreExclusive"] = stones["RunningScoreExclusive"]
-training_data["PowerPlay"] = stones["PowerPlay"]
-
-print(training_data)
-
-# dataset = tf.data.TextLineDataset("dataset/Stones.csv")
-# for element in dataset:
-    # print(element)
-
-print("Hello")
 
 """
 EndID               x
@@ -77,7 +12,48 @@ Powerplay
 ? PlayerID
 ? Timeout
 
+
+"0": "Draw"
+"1": "Front"
+"2": "Guard"
+"3": "Raise / Tap-back"
+"4": "Wick / Soft Peeling"
+"5": "Freeze"
+"6": "Take-out"
+"7": "Hit and Roll"
+"8": "Clearing"
+"9": "Double Take-out"
+"10": "Promotion Take-out"
+"11": "through"
+"13": "no statistics"
+
 > Type
 > Handle
 > Points
 """
+
+from load_data import load_data
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+
+# def make_assignment():
+#     f = random.random()
+#     if f < 0.8:
+#         return "TRAIN"
+#     elif f < 0.9:
+#         return "DEV"
+#     else:
+#         return "TEST"
+
+# all_games = dataset[["CompetitionID","SessionID","GameID"]].drop_duplicates()
+# all_games["Group"] = [make_assignment() for _ in range(len(all_games)) ]
+# print(all_games)
+
+# all_games.to_csv("training_groups.csv")
+
+dataset = load_data()
+training_groups = pd.read_csv("training_groups.csv")
+dataset = pd.merge(dataset, training_groups, how="left", on=["CompetitionID","SessionID","GameID"])
+
+print(dataset)

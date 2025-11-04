3d4879d9c124a29501ec158f34ee06ff95b17564
Dragon-Hatcher <danieldragonhatcher@gmail.com>
2025-11-04 13:50:31 -0800

    Keras

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 
+
 # def make_assignment():
 #     f = random.random()
 #     if f < 0.8:
@@ -55,9 +56,15 @@ dataset = load_data()
 training_groups = pd.read_csv("training_groups.csv")
 dataset = pd.merge(dataset, training_groups, how="left", on=["CompetitionID","SessionID","GameID"])
 
-print(dataset)
-
 # 57 ---ReLu-> 50 ---ReLu-> 30 ---softmax-> 12
+
+def make_model(input_features):
+    input = tf.keras.Input(shape=(input_features,))
+    A1 = tf.keras.layers.Dense(50, activation='relu')(input)
+    A2 = tf.keras.layers.Dense(30, activation='relu')(A1)
+    A3 = tf.keras.layers.Dense(12, activation='softmax')(A2)
+    model = tf.keras.Model(inputs=input, outputs=A3)
+    return model
 
 def initialize_params():
     IN = 43
@@ -191,5 +198,27 @@ train_data_Y = train_dataset[OUTPUT_COLS].to_numpy().astype('float32')
 dev_data_X = dev_dataset[INPUT_COLS].to_numpy().astype('float32')
 dev_data_Y = dev_dataset[OUTPUT_COLS].to_numpy().astype('float32')
 
-model(train_data_X, train_data_Y, dev_data_X, dev_data_Y)
+# model(train_data_X, train_data_Y, dev_data_X, dev_data_Y)
 
+print()
+print("----------")
+print()
+
+model = make_model(train_data_X.shape[1])
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+tf_train_dataset = tf.data.Dataset.from_tensor_slices((train_data_X, train_data_Y)).batch(128)
+tf_dev_dataset = tf.data.Dataset.from_tensor_slices((dev_data_X, dev_data_Y)).batch(128)
+
+class_counts = np.array([train_dataset[col].sum() for col in OUTPUT_COLS])
+class_weights = class_counts.sum() / (len(class_counts) * class_counts)
+class_weight_dict = {i: w for i, w in enumerate(class_weights)}
+
+print(class_weight_dict)
+
+history = model.fit(tf_train_dataset, epochs=20, validation_data=tf_dev_dataset,)
+
+print(pd.DataFrame(dev_data_Y[0:10,:]))
+p = model.predict(dev_data_X[0:10,:])
+print(pd.DataFrame(p))
